91860967de792506e50eeb552f2621ada39f97e3
Bards <mbardwell@users.noreply.github.com>
2018-11-29 13:47:38 -0700

    Power flow sim made independent from ETX libraries and tested. Full functionality.

--- powerflowsim.py
+++ powerflowsim.py
@@ -1,30 +1,22 @@
 """
 Power system load flow (PSLF) simulation code by Mike Bardwell, MSc
+The term node(s) is used interchangeably with participant(s)
 University of Alberta, 2018
 """
 
-import os
-import sys
 import logging
 import matplotlib.pyplot as plt
 import numpy as np
 import pypsa
 import powersystemnetwork as psn
 
-sys.path.append('../')
-from simulator import Simulation # imports from exchange library
-
 logging.basicConfig(level=logging.ERROR) # must be called before pypsa
 pypsa.pf.logger.setLevel(logging.ERROR) # Use ERROR or DEBUG
-
-
-
 
 class PowerFlowSim():
     """Builds PyPSA-based power system load flow simulation."""
 
-    def __init__(self, length, topology='web',
-                 subpath='../_configs/3node.json'):
+    def __init__(self, length, filename='./data/3node.json'):
         """
         type: length: int. Length of usable load profile
         type: topology: String. System network topology (default 'web')
@@ -32,29 +24,15 @@ class PowerFlowSim():
         (default '../_configs/3node.json')
         """
 
-        filename = os.path.join(os.path.dirname(__file__), subpath)
         self.length = length
-        self.topology = topology
         self.network_param = psn.Network(filename)
-        self.network_param_old = Simulation(filename)
         self.sampleperiod = 60 # default for all files in use
         self.pypsa_network = pypsa.Network(sort=False) # initialise network
-        self.pypsa_network.set_snapshots(range(self.__adjustLengths(length)))
-
-        self.nodeloads = np.array([])
-        self.nodevoltages = np.array([])
+        self.pypsa_network.set_snapshots(range(self._adjustLengths(length)))
 
     def noAgents(self):
         """Determines number of network participants."""
-
-        return len(self.network_param_old.participants)
-
-    def attributeCheck(self):
-        """TO DO: Implement storage."""
-
-        for agent in self.network_param_old.participants:
-            if agent.devices['storage'] is not None:
-                print('Agent', agent.id, 'has storage')
+        return len(self.network_param.participants)
 
     def setupBuses(self):
         """Adds buses to network.
@@ -62,131 +40,114 @@ class PowerFlowSim():
         Important: MW and EUR are default units
         """
 
-        for agent in self.network_param_old.participants:
-            self.network.add("Bus", agent.id, v_nom=20.)
+        nodes = self.network_param.participants # shorthanding
+        for bus in nodes:
+            self.pypsa_network.add("Bus", bus, v_nom=20.)
 
     def setupGenerators(self):
         """Adds generators to network."""
 
-        for agent in self.network_param_old.participants:
-            if agent.devices['generation'] is None \
-               or agent.devices['generation'] == []:
+        nodes = self.network_param.participants # shorthanding
+        for bus in nodes:
+            if nodes[bus].gen is None:
                 continue
             else:
-                self.network.add(
-                    "Generator", "{}-{}".format(agent.id, "Gas"),
-                    bus=agent.id,
+                self.pypsa_network.add(
+                    "Generator", "{}-{}".format(bus, "Gas"),
+                    bus=bus,
                     p_nom=0.005, # Nominal household power is 5 kW
                     marginal_cost=0, # REMOVE?
-                    p_max_pu=agent.devices['generation']. \
-                               offline_profile['profile'],
+                    p_max_pu=nodes[bus].gen['profile']
                     )
 
     def setupLoads(self):
         """Adds loads to network"""
 
-        for agent in self.network_param_old.participants:
-            if agent.devices['load'] is None:
+        nodes = self.network_param.participants # shorthanding
+        for bus in nodes:
+            if nodes[bus].load is None:
                 continue
             else:
-                self.network.add("Load",
-                                 "{}-Load".format(agent.id),
-                                 bus=agent.id,
-                                 p_set=agent.devices['load']. \
-                                 offline_profile['profile'])
+                self.pypsa_network.add("Load",
+                                       "{}-Load".format(bus),
+                                       bus=bus,
+                                       p_set=nodes[bus].load['profile']
+                                       )
 
-    def setupLines(self, xlines=0.1, rlines=0.1):
+    def setupLines(self, topology='web', x_lines=0.1, r_lines=0.1):
         """Adds power lines to network"""
 
-        usedagentlist = []
-        if self.topology == 'web':
-            for agent in self.network_param_old.participants:
-                for otheragent in self.network_param_old.participants:
-                    if ((otheragent.id != agent.id) and not (np.any(
-                            [otheragent.id == x for x in usedagentlist]))):
-                        self.network.add(
-                            "Line",
-                            "{}-{}".format(agent.id, otheragent.id),
-                            bus0=agent.id,
-                            bus1=otheragent.id,
-                            x=xlines,
-                            r=rlines)
-                    usedagentlist.append(agent.id)
-        elif self.topology == 'ring':
-            for agent in self.network_param_old.participants:
-                if usedagentlist == []:
-                    self.network.add(
-                        "Line", "{}-{}".
-                        format(self.network_param_old.participants[0].id,
-                               self.network_param_old.participants[-1].id),
-                        bus0=self.network_param_old.participants[0].id,
-                        bus1=self.network_param_old.participants[-1].id,
-                        x=xlines,
-                        r=rlines)
-                    usedagentlist.append(agent.id)
+        def _addLine(bus, other_bus, x_line=x_lines, r_line=r_lines):
+            self.pypsa_network.add("Line",
+                                   "{}-{}".format(bus, other_bus),
+                                   bus0=bus,
+                                   bus1=other_bus,
+                                   x=x_line,
+                                   r=r_line)
+
+        nodes = self.network_param.participants # shorthanding
+        if topology == 'web':
+            usedbuslist = []
+            for bus in nodes:
+                for other_bus in nodes:
+                    if ((other_bus != bus) and not (np.any(
+                            [other_bus == x for x in usedbuslist]))):
+                        _addLine(bus, other_bus)
+                    usedbuslist.append(bus)
+        elif topology == 'ring':
+            first_bus = None
+            last_bus = None
+            for bus in nodes:
+                if last_bus is None:
+                    first_bus = bus
+                    last_bus = bus
                     continue
                 else:
-                    self.network.add(
-                        "Line",
-                        "{}-{}".format(agent.id, usedagentlist[-1]),
-                        bus0=agent.id,
-                        bus1=usedagentlist[-1],
-                        x=xlines,
-                        r=rlines)
-                    usedagentlist.append(agent.id)
-        elif self.topology == 'radial':
-            agent = self.network_param_old.participants.copy() # short var
-            add = self.network.add
+                    _addLine(bus, last_bus)
+                    last_bus = bus
+                _addLine(last_bus, first_bus)
+
+        elif topology == 'radial':
             j = 1
-            for i in range(round(len(self.network_param_old.participants)/3)):
+            for i in range(round(len(nodes)/3)):
                 for _ in range(3):
-                    if j != len(self.network_param_old.participants):
-                        add("Line",
-                            "{}-{}".format(agent[i].id,
-                                           agent[j].id),
-                            bus0=agent[i].id,
-                            bus1=agent[j].id,
-                            x=xlines,
-                            r=rlines)
+                    if j != len(nodes):
+                        _addLine(nodes.keys()[i], nodes.keys()[j])
                         j += 1
 
     def lengthCap(self):
-        """Trims length of load and generation profiles."""
+        """Trims length of load and gen profiles to min common length."""
 
         min_length = float("inf")
-        for agent in self.network_param_old.participants:
-            if agent.devices['load'] is not None:
-                if len(agent.devices['load']. \
-                offline_profile['profile']) < min_length:
-                    min_length = len(agent.devices['load']. \
-                    offline_profile['profile'])
-            if agent.devices['generation'] is not None:
-                if len(agent.devices['generation']. \
-                offline_profile['profile']) < min_length:
-                    min_length = len(agent.devices['generation']. \
-                    offline_profile['profile'])
-        self.__adjustLengths(min_length)
+        nodes = self.network_param.participants # shorthanding
+        for bus in nodes:
+            if nodes[bus].load is not None:
+                if len(nodes[bus].load['profile']) < min_length:
+                    min_length = len(nodes[bus].load['profile'])
+            if nodes[bus].gen is not None:
+                if len(nodes[bus].gen['profile']) < min_length:
+                    min_length = len(nodes[bus].gen['profile'])
+        self._adjustLengths(min_length)
         return min_length
 
-    def __adjustLengths(self, length):
-        for agent in self.network_param_old.participants:
-            if agent.devices['load'] is not None:
-                agent.devices['load'].offline_profile['profile'] = \
-                agent.devices['load']. \
-                offline_profile['profile'][0:length]
-            if agent.devices['generation'] is not None:
-                agent.devices['generation'].offline_profile['profile'] \
-                = agent.devices['generation']. \
-                offline_profile['profile'][0:length]
+    def _adjustLengths(self, length):
+
+        nodes = self.network_param.participants # shorthanding
+        for bus in nodes:
+            if nodes[bus].load is not None:
+                nodes[bus].load['profile'] = \
+                nodes[bus].load['profile'][0:length]
+            if nodes[bus].gen is not None:
+                nodes[bus].gen['profile'] = nodes[bus].gen['profile'][0:length]
         return length
 
     def plotLoads(self, save=False, style='-'):
         """Plots load profiles."""
 
         plot = []
-        for name in self.network.loads_t.p.keys():
-            plot.extend(plt.plot(self.network.loads_t.p[name],
-                                 style, label=name))
+        for bus in self.pypsa_network.loads_t.p.keys():
+            plot.extend(plt.plot(self.pypsa_network.loads_t.p[bus],
+                                 style, label=bus))
         plt.legend(handles=plot)
         plt.title('Loading Data Over Time')
         plt.xlabel('Iteration (in ' + str(self.sampleperiod)
@@ -200,9 +161,9 @@ class PowerFlowSim():
         """Plots generator profiles."""
 
         plot = []
-        for name in self.network.generators_t.p.keys():
-            plot.extend(plt.plot(self.network.generators_t.p[name],
-                                 style, label=name))
+        for bus in self.pypsa_network.generators_t.p.keys():
+            plot.extend(plt.plot(self.pypsa_network.generators_t.p[bus],
+                                 style, label=bus))
         plt.legend(handles=plot)
         plt.title('Generation Power Over Time')
         plt.xlabel('Iteration')
@@ -218,19 +179,19 @@ class PowerFlowSim():
         else:
             timestamps = no_timestamps
         if node:
-            name = self.network.buses_t['v_mag_pu'].keys()[node]
-            plotdata = plt.plot(self.network.buses_t.
-                                v_mag_pu[name][0:timestamps])
+            bus = self.pypsa_network.buses_t['v_mag_pu'].keys()[node]
+            plotdata = plt.plot(self.pypsa_network.buses_t.
+                                v_mag_pu[bus][0:timestamps])
             plt.xlabel('Timestamps')
             plt.ylabel('Node Voltages')
             plt.title('blah')
             plt.show()
             return plotdata[0].get_ydata(True)
         else:
-            for name in self.network.buses_t['v_mag_pu'].keys():
+            for bus in self.pypsa_network.buses_t['v_mag_pu'].keys():
                 plot.extend(plt.plot(
-                    self.network.buses_t.v_mag_pu[name],
-                    style, label=name))
+                    self.pypsa_network.buses_t.v_mag_pu[bus],
+                    style, label=bus))
             plt.legend(handles=plot)
             plt.title('Node Voltages Over Time')
             plt.xlabel('Iteration')
@@ -242,9 +203,9 @@ class PowerFlowSim():
         """Plots power line loading."""
 
         plot = []
-        for name in self.network.lines_t.p0.keys():
-            plot.extend(plt.plot(self.network.lines_t.p0[name],
-                                 style, label=name))
+        for bus in self.pypsa_network.lines_t.p0.keys():
+            plot.extend(plt.plot(self.pypsa_network.lines_t.p0[bus],
+                                 style, label=bus))
         plt.legend(handles=plot)
         plt.title('Line Loading Over Time')
         plt.xlabel('Iteration')
@@ -255,12 +216,12 @@ class PowerFlowSim():
         """Plots voltage magnitudes per node in box plot form"""
 
         data, toremove = [], []
-        keys = self.network.buses_t['v_mag_pu'].keys().tolist()
-        for name in keys:
-            if self.network.buses['control'][name] == 'Slack':
-                toremove.append(name)
+        keys = self.pypsa_network.buses_t['v_mag_pu'].keys().tolist()
+        for bus in keys:
+            if self.pypsa_network.buses['control'][bus] == 'Slack':
+                toremove.append(bus)
                 continue
-            data.append(self.network.buses_t.v_mag_pu[name])
+            data.append(self.pypsa_network.buses_t.v_mag_pu[bus])
         [keys.remove(x) for x in toremove]
         plt.subplot(121)
         plt.boxplot(data, labels=keys)
@@ -281,32 +242,31 @@ class PowerFlowSim():
     def toArrays(self, load=False, voltage=False):
         """Create numpy arrays with load and voltage profiles"""
 
+        self.nodeloads = np.array([])
+        self.nodevoltages = np.array([])
         if load:
-            for name in self.network.loads_t.p.keys():
+            for bus in self.pypsa_network.loads_t.p.keys():
                 self.nodeloads = np.append(self.nodeloads,
-                                           self.network.loads_t.p[name]
+                                           self.pypsa_network.loads_t.p[bus]
                                           )
             self.nodeloads = self.nodeloads.reshape(
-                len(self.network.loads_t.p.keys()), self.length).T
+                len(self.pypsa_network.loads_t.p.keys()),
+                self.length).T
 
 
         if voltage:
-            for name in self.network.buses_t['v_mag_pu'].keys():
+            for bus in self.pypsa_network.buses_t['v_mag_pu'].keys():
                 self.nodevoltages = np.append(
                     self.nodevoltages,
-                    self.network.buses_t.v_mag_pu[name]
+                    self.pypsa_network.buses_t.v_mag_pu[bus]
                     )
             self.nodevoltages = self.nodevoltages.reshape(
-                len(self.network.buses_t['v_mag_pu'].keys()),
+                len(self.pypsa_network.buses_t['v_mag_pu'].keys()),
                 self.length).T
-
-    def getCurrentTime(self):
-        """Access network timestamp"""
-
-        return self.network_param_old.exchange.time()
 
     def nrPfSim(self, showall=False):
         """Base case for testing power flow class"""
+
         self.setupBuses()
         self.setupGenerators()
         self.setupLoads()
