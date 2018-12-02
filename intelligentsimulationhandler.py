a4f8131eb23ac2ed7bc0487a25076be971fbff4f
Bards <mbardwell@users.noreply.github.com>
2018-12-02 11:54:47 -0700

    Gathers json_config files for comparisons

--- intelligentsimulationhandler.py
+++ intelligentsimulationhandler.py
@@ -3,14 +3,36 @@ Intelligent simulation handler (ISH) code by Mike Bardwell, MSc
 University of Alberta, 2018
 """
 
-import matplotlib.pyplot as plt
-import numpy as np
+import os
+from powersystemnetwork import Network
 
-
-class IntelligentSimulationHandler(object):
-    """Runs power system load flow analysis using numerical methods or
-       machine learning
+class IntelligentSimulationHandler():
+    """Decides on whether to use look-up table or run a new power system
+       load flow simulation
     """
     
-    def __init__(self):
+    def __init__(self, json_config):
+        self.network = Network(json_config, False)
+        self.config_files = self.gatherFiles()
+        self.totalComparison()
         
+    def gatherFiles(self):
+        """Searches data folder for json files"""
+        
+        config_files = []
+        for root, _, files in os.walk('./data/'):
+            for file in files:
+                if file.endswith('.json'):
+                    config_files.append(root + file)
+        return config_files
+    
+    def totalComparison(self):
+        """Compares json files in data folder to inputted json config file"""
+        
+        for file in self.config_files:
+            x = Network(file, False).config
+            y = self.network.config
+            shared_items = {k: x[k] for k in x if k in y and x[k] == y[k]}
+            print(file, len(shared_items))
+        
+ish = IntelligentSimulationHandler('./data/3node.json')
