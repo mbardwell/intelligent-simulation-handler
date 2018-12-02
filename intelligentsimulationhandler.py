8ad318bbf284f02e5f8a880911794b3c709af65c
Bards <mbardwell@users.noreply.github.com>
2018-12-02 13:35:45 -0700

    Files should only be scanned once. Within each scan, the full comparison suit is run. Here I setup totalComparison to act as the iterator, which calls all of the comparitor methods

--- intelligentsimulationhandler.py
+++ intelligentsimulationhandler.py
@@ -14,7 +14,7 @@ class IntelligentSimulationHandler():
     def __init__(self, json_config):
         self.network = Network(json_config, False)
         self.config_files = self.gatherJsonFiles()
-        self.compareConnections()
+        self.totalComparison()
 
     def gatherJsonFiles(self):
         """Searches data folder for json files"""
@@ -26,35 +26,34 @@ class IntelligentSimulationHandler():
                     config_files.append(root + file)
         return config_files
 
-    def compareConnections(self, threshold=0.95):
+    def compareConnections(self, x, threshold=0.95):
         """Compares power system network connections"""
 
-        for file in self.config_files.copy():
-            try:
-                x = Network(file, False).config['connections']
-            except:
-                self.config_files.remove(file)
-                continue
+        try:
+            x = x['connections']
             y = self.network.config['connections']
+        except:
+            return False
 
-            bigger_len = len(x) if len(x) > len(y) else len(y)
-            smaller_len = len(x) if bigger_len == len(y) else len(y)
-            no_similarities = 0
-            no_possible_connections = (len(x)*(len(x)-1))/2
-            for i in range(smaller_len):
-                for j in range(i+1, smaller_len):
-                    if x[i][j] == y[i][j]:
-                        no_similarities += 1
-            if (no_similarities/no_possible_connections) < threshold:
-                self.config_files.remove(file)
+        bigger_len = len(x) if len(x) > len(y) else len(y)
+        smaller_len = len(x) if bigger_len == len(y) else len(y)
+        no_similarities = 0
+        no_possible_connections = (len(x)*(len(x)-1))/2
+        for i in range(smaller_len):
+            for j in range(i+1, smaller_len):
+                if x[i][j] == y[i][j]:
+                    no_similarities += 1
+        if (no_similarities/no_possible_connections) < threshold:
+            return False
+        else:
+            return True
 
     def totalComparison(self):
         """Compares json files in data folder to inputted json config file"""
 
-        for file in self.config_files:
+        for file in self.config_files.copy():
             x = Network(file, False).config
             y = self.network.config
             shared_items = {k: x[k] for k in x if k in y and x[k] == y[k]}
-            print(file, len(shared_items))
 
 ish = IntelligentSimulationHandler('./data/3node.json')
