ce208721442fa4c2e19abcd9ba8cf95c08171311
Bards <mbardwell@users.noreply.github.com>
2018-12-03 12:27:20 -0700

    Added connection matrix generation code. Updated paths to ../data/

--- utils/montecarlo.py
+++ utils/montecarlo.py
@@ -35,26 +35,20 @@ def generateLoadProfile(length = 100, amplitude = 2):
 
 def generateMonteCarloBinaries(filename, starting_no = 0, 
                                no_files = 1, amplitude = 2,
-                               computer = 'mikelaptop', writeoverfile = False):
+                               writeoverfile = False):
     """Generates Monte Carlo binary files for power system load flow sim.
     
     type: filename: String. Should be the file you want to copy-modify
-                    ex: 'C:/Users/mikey/Downloads/zp/18356'
+                    ex: '../data/18356'
     type: no_files: int. Number of files to create
     type: starting_no: int. First suffix of generated monte carlo file
     type: amplitude: int. Generate rand profile between [0, amplitude]
-    type: computer: String. Either 'mikelaptop' or 'mikework'
     """
     from pathlib import Path
     
-    if computer == 'mikework':
-        sub_dump_path = 'C:/Users/mikey/Downloads/zp/montecarlo'
-    else:
-        sub_dump_path = 'C:/Users/Michael/Downloads/zp/montecarlo'
-    
     network_data = importZp(filename)
     for i in range(no_files):
-        dump_path = sub_dump_path + str(starting_no + i)
+        dump_path = '../data/montecarlo' + str(starting_no + i)
         if not Path(dump_path + '.zp').is_file() or writeoverfile:
             network_data['load']['profile'] = generateLoadProfile(5e3, 
                                                                   amplitude)
@@ -74,31 +68,23 @@ def viewGenerationProfile(filename):
     plt.plot(sim['gen']['profile'])
         
 
-def generateJson(nohouses, computer = 'mikelaptop', auto_proceed = False):
+def generateJson(no_houses, topology='radial', auto_proceed = False):
     """Generates JSON file requried for power flow study.
     
-    type: nohouses: int. Number of houses in monte carlo study
-    type: computer: String. Either 'mikelaptop' or 'mikework'
+    type: no_houses: int. Number of houses in monte carlo study
     type: auto_proceed: boolean. If True, binaries with be auto-created
                         WARNING - many binary files can require GB's of memory
     """
-    def fileExists(nohouses):
+    def fileExists(no_houses):
         """Checks two things. If JSON file and required binaries exist.
         
-        type: nohouses: int. Number of houses in monte carlo study
+        type: no_houses: int. Number of houses in monte carlo study
         """
         from pathlib import Path
         
         proceed_flag = auto_proceed
-        for i in range(nohouses):
-            if computer == 'mikework':
-                sub_path = 'C:/Users/mikey/Downloads/zp/'
-            elif computer == 'mikelaptop':
-                sub_path = 'C:/Users/Michael/Downloads/zp/'
-            else:
-                print('ERROR: File path not recognized')
-                sys.exit(1)
-            file_path = Path(sub_path + 'montecarlo' + str(i) + '.zp')
+        for i in range(no_houses):
+            file_path = Path('../data/' + 'montecarlo' + str(i) + '.zp')
             
             if not file_path.is_file() and not proceed_flag:
                 if input('WARN: Will create binaries. Proceed? Y/N ') == 'Y':
@@ -107,11 +93,9 @@ def generateJson(nohouses, computer = 'mikelaptop', auto_proceed = False):
                     print('Binaries not available. Process aborted')
                     sys.exit(1)
             if proceed_flag == True:
-                _computer = computer # avoids computer = computer below
-                generateMonteCarloBinaries((sub_path + '18356'), i,
-                                           computer = _computer)
+                generateMonteCarloBinaries('../data/' + '18356', i)
 
-        my_file = Path('../../_configs/montecarlo' + str(nohouses) + '.json')
+        my_file = Path('../data/montecarlo' + str(no_houses) + '.json')
         if my_file.is_file():
             return True
         else: 
@@ -125,25 +109,54 @@ def generateJson(nohouses, computer = 'mikelaptop', auto_proceed = False):
         """
         profile = {
                     "id" : str(fileid),
-                    "priority" : 2,
                     "logic_type" : "binary",
-                    "load": {
-                            "scale": 1
-                            },
-                    "generation": {
-                            "available": generation_flag,
-                            "scale": {	
-                                    "type": "normal",
-                                    "scale": 1
-                                     }
-                                 },
+                    "load": True,
+                    "generation": generation_flag,
                     "storage": False
                   }
         return profile
+    
+    def generateConnections(topology='radial'):
+        """Generates connection topology for power system load flow
+           montecarlo json_config files
+        """
         
-    if not fileExists(nohouses):
+        connection_matrix = []
+        if topology == 'radial':
+            radial_constant = 3
+            for i in range(no_houses):
+                connection_rows = []
+                for j in range(no_houses):
+                    if i == j or j < radial_constant*i+1 or j > radial_constant*(i+1):
+                        connection_rows.append(0)
+                    else:
+                        connection_rows.append(1)
+                connection_matrix.append(connection_rows)
+        if topology == 'web':
+            for i in range(no_houses):
+                connection_rows = []
+                for j in range(no_houses):
+                    if i == j: 
+                        connection_rows.append(0)
+                    else:
+                        connection_rows.append(1)
+                connection_matrix.append(connection_rows)
+        if topology == 'ring':
+            for i in range(no_houses):
+                connection_rows = []
+                for j in range(no_houses):
+                    if (j == i+1 or j == i-1 or j == i+no_houses-1 
+                        or j == i-no_houses+1): 
+                        connection_rows.append(1)
+                    else:
+                        connection_rows.append(0)
+                connection_matrix.append(connection_rows)
+            
+        return connection_matrix
+    
+    if not fileExists(no_houses):
         profiles = []
-        for i in range(nohouses):
+        for i in range(no_houses):
             if i == 0:
                 # First profile should include generation
                 profiles.append(generateProfile('montecarlo' + str(i), True))
@@ -152,22 +165,14 @@ def generateJson(nohouses, computer = 'mikelaptop', auto_proceed = False):
         
         # start_datetime entry to be replaced with below eventually
         # str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        if computer == 'mikework':
-            profile_path = "C:/Users/mikey/Downloads/zp/" # work computer
-        elif computer == 'mikelaptop':
-            profile_path = "C:/Users/Michael/Downloads/zp/" # laptop
-        else:
-            print('ERROR: File path not recognized')
-            exit(0)
-        data = {"profile_path" : profile_path, 
+        data = {"profile_path" : './data/', 
                 "start_datetime" : '2016-04-01 0:0:0',
-                "study": {
-                        "description": "binary2-m4y16-super-load"
-                         },
+                "study": 'TODO: Prompt ISH user for name of study',
+                "lookup_table": False,
+                "connections": generateConnections(topology),
                 "profiles": profiles
                 }
                 
-        with open('../../_configs/montecarlo' + str(nohouses) + '.json', 'w')\
+        with open('../data/montecarlo' + str(no_houses) + '.json', 'w')\
         as f:
             json.dump(data, f, indent = 2)
-            
