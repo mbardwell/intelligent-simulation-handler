c79afdf632f5bd2a76b4c97d24b39de1647fcc4e
Bards <mbardwell@users.noreply.github.com>
2018-12-05 15:52:51 -0700

    Added save/load features to normal equation class. Reorganized

--- regressiontools.py
+++ regressiontools.py
@@ -4,6 +4,8 @@ Linear regression tools for Power Flow emulation
 
 @author: Michael Bardwell, University of Alberta, Edmonton AB CAN
 """
+
+import sys
 import tensorflow as tf
 from tensorflow import keras
 from keras.models import Sequential
@@ -16,27 +18,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime
 import json
+import h5py
 
 class TrainANN(object):
+    """Trains feedforward ANN using power system load flow load/voltage data"""
+    
     def __init__(self, load_profile=None, voltage_profile=None, 
                  train_percentage=0.7, learning_rate=0.001, no_hidden_layers=1,
                  layer_density=64, dropout=False, no_epochs=1000,
                  early_stop=True, save_model=False,
                  plot_results=False):
-        """
-        :type load_profile: List[int], voltage_profile: List[int]
-        :type train_percentage: int, name: String
-        """
-        _split_index = int(train_percentage * len(load_profile))
-        self.train_data = load_profile[0:_split_index]
-        self.train_labels = voltage_profile[0:_split_index]
-        
-        self.test_data = load_profile[_split_index+1:]
-        self.test_labels = voltage_profile[_split_index+1:]
-        
-        if load_profile is None or voltage_profile is None:
-            self.loadModel()
-        else:
+
+        if load_profile is not None and voltage_profile is not None:
+            _split_index = int(train_percentage * len(load_profile))
+            self.train_data = load_profile[0:_split_index]
+            self.train_labels = voltage_profile[0:_split_index]
+            
+            self.test_data = load_profile[_split_index+1:]
+            self.test_labels = voltage_profile[_split_index+1:]
+            
             self.buildModel(learning_rate, no_hidden_layers, layer_density,
                             dropout)
             self.trainModel(no_epochs, early_stop)
@@ -116,17 +116,6 @@ class TrainANN(object):
             plt.xlabel("Prediction Error")
             plt.ylabel("Count")
             plt.show()
-            
-    def saveModel(self, name='annmodel'):
-        ## TO DO: Test this function
-        # serialize model to JSON
-        model_json = self.model.to_json()
-        with open('./data/lookup_tables/' + name + ".json", "w") as file:
-            json.dump(model_json, file)
-            
-        # serialize weights to HDF5
-        self.model.save_weights('./data/lookup_tables/' + name + ".h5")
-        print("Saved model to disk")
 
     def plotHistory(self):
         plt.figure()
@@ -139,12 +128,109 @@ class TrainANN(object):
         plt.legend()
         plt.ylim([0,0.2])
         plt.show()
+
+    def saveModel(self, name='annmodel'):
+        ## TO DO: Test this function
+        model_json = self.model.to_json()
+        with open('./data/lookup_tables/' + name + ".json", "w") as file:
+            json.dump(model_json, file)
+        file.close()
+            
+        # serialize weights to HDF5
+        self.model.save_weights('./data/lookup_tables/' + name + ".h5")
         
-    def loadModel(self):
-        print('load model')
+    def loadModel(self, model_name):
+        """Decodes a JSON file into a keras model"""
         
+        path = './data/lookup_tables/'
+        try:
+            with open(path + model_name + '.json', 'r') as ann_model_json:
+                model_json_string = ann_model_json.read().\
+                replace('\\', '')[1:-1]
+                model = model_from_json(model_json_string)
+            ann_model_json.close()
+            model.load_weights(path + model_name + '.h5', by_name=False)
+            print('Opening ANN-derived look up table')
+            return model
+        except BaseException as ex:
+            print('Line {} - lookup table loading failed. {}'.format(
+                    sys.exc_info()[2].tb_lineno, ex))
+            return False
+
+
+class NormalEquation(object):
+    """Trains parametric algorithm using power system load flow load/voltage 
+       data
+    """
+
+    def __init__(self, load_profile=None, voltage_profile=None, 
+                 save_model=False):
+        
+        if load_profile is not None and voltage_profile is not None:
+            self.lpf = {'data': load_profile, 'target': voltage_profile}
+            m = self.lpf['target'].shape[0]
+            self.lpf['data_pb'] = np.c_[np.ones((m, 1)), self.lpf['data']]
+            self.calculateTheta()
+            self.calculateBias()
+            self.predictWithModel(load_profile)
+            self.evaluateModel()
+            
+            if save_model:
+                self.model_name = 'ne_model_' + \
+                str(datetime.datetime.now()).\
+                replace(':', '-').replace(' ', '_')
+                self.saveModel(self.model_name)
+        
+    def calculateTheta(self):
+        x = tf.constant(self.lpf['data_pb'], 
+                           dtype = tf.float32, name = "x")
+        y = tf.constant(self.lpf['target'], 
+                           dtype = tf.float32, name = "y") 
+        xt = tf.transpose(x)
+        theta = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(xt, x)), xt), 
+                          y)
+        with tf.Session(): ## do I even need this??
+            self.theta = theta.eval()
+            
+    def calculateBias(self):
+        self.bias = [self.theta[0] for i in range(self.lpf['target'].shape[0])]
+        
+    def predictWithModel(self, load_profile):
+        return np.dot(load_profile, self.theta[1:].T) + self.bias
+        
+    def evaluateModel(self, plot_results=True):
+        #TODO: plot results
+        prediction = self.predictWithModel(self.lpf['data'])
+        mse = np.mean((prediction.T[1:].T - self.lpf['target'].T[1:].T)**2)
+        mae = abs(np.mean((prediction.T[1:].T - self.lpf['target'].T[1:].T)))
+        print('normal equation mse: {}, mae: {}'.format(mse, mae))
+        return mae
+    
+    def saveModel(self, model_name):
+        """Encodes parametric model parameters into HDF5 binary data format"""
+        
+        path = './data/lookup_tables/'
+        with h5py.File(path + model_name + '.h5', 'w') as f:
+            f.create_dataset(name='data', data=np.array(self.theta))
+        f.close()
+
+    def loadModel(self, model_name):
+        """Decodes HDF5 binary data format into parametric model parameters"""
+        
+        path = './data/lookup_tables/'
+        try:
+            self.theta = h5py.File(path + model_name + '.h5', 'r')['data']
+            print('Opening NE-derived look up table')
+            return self.theta
+        except BaseException as ex:
+            print('Line {} - lookup table loading failed. {}'.format(
+                    sys.exc_info()[2].tb_lineno, ex))
+            return False
+
         
 class TrainRNN(object):
+    """TODO: Proof of concept only right now"""
+    
     def __init__(self, load_profile, voltage_profile, train_percentage = 0.7, name = 'ann_model'):
         """
         :type load_profile: List[int], voltage_profile: List[int]
@@ -198,37 +284,3 @@ class TrainRNN(object):
         [loss, mae] = self.model.evaluate(self.test_data, self.test_labels, verbose=0)
         print("ANN regression loss: {}, mae: {}".format(loss, mae))
         return mae
-
-
-class NormalEquation(object):
-    def __init__(self, load_profile, voltage_profile):
-        self.lpf = {'data': load_profile, 
-                    'target': voltage_profile}
-        mlpf = self.lpf['target'].shape[0]
-        self.lpf['data_pb'] = np.c_[np.ones((mlpf, 1)), self.lpf['data']]
-        
-    def calculateTheta(self):
-        xlpf = tf.constant(self.lpf['data_pb'], 
-                           dtype = tf.float32, name = "xlpf")
-        ylpf = tf.constant(self.lpf['target'], 
-                           dtype = tf.float32, name = "ylpf") 
-        xlpft = tf.transpose(xlpf)
-        thetalpf = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(xlpft, xlpf)), xlpft), ylpf)
-        with tf.Session(): ## do I even need this??
-            self.thetalpf_value = thetalpf.eval()
-            
-    def calculateBias(self):
-        biaslpf = []
-        for i in range(self.lpf['target'].shape[0]):
-            biaslpf.append(self.thetalpf_value[0])
-        self.biaslpf = np.array(biaslpf)
-        
-    def predictWithModel(self, load_profile):
-        return np.dot(load_profile, self.thetalpf_value[1:].T) + self.biaslpf
-        
-    def evaluateModel(self, plot_results = True):
-        prediction = self.predictWithModel(self.lpf['data'])
-        mselpf = np.mean((prediction.T[1:].T - self.lpf['target'].T[1:].T)**2)
-        maelpf = abs(np.mean((prediction.T[1:].T - self.lpf['target'].T[1:].T)))
-        print('normal equation mse: {}, mae: {}'.format(mselpf, maelpf))
-        return maelpf
